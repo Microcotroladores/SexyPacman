0571ac8dc86ecd70933d69425a06c09e2b83e314
Praxedis Augusto Paloalto Gómez <paurguastx@gmail.com>
2014-06-01 20:47:59 -0500

    Actualización de auxiliares

--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -6,8 +6,8 @@
 CND_BASEDIR=`pwd`
 # default configuration
 CND_ARTIFACT_DIR_default=dist/default/production
-CND_ARTIFACT_NAME_default=pacman10.X.production.hex
-CND_ARTIFACT_PATH_default=dist/default/production/pacman10.X.production.hex
+CND_ARTIFACT_NAME_default=SexyPacman.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/SexyPacman.production.hex
 CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
-CND_PACKAGE_NAME_default=pacman10.x.tar
-CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/pacman10.x.tar
+CND_PACKAGE_NAME_default=sexypacman.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/sexypacman.tar
